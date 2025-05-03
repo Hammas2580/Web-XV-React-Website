417f1f4f5c3d2ec12ba4f9e5e75164a816b1346d
HashamSalauddin2006 <your_email@example.com>
2025-05-03 19:49:13 +0500

    Add Tille name & Logo

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,28 @@ import Slider from "./Components/SliderSection/Slider";
 import About from "./Components/AboutSection/About";
 import Chart from "./Components/ChartSection/Chart";
 import Token from "./Components/TokenSection/Token";
-import  Footer from "./Components/FooterSection/Footer";
+import Footer from "./Components/FooterSection/Footer";
+import { Route, Routes } from "react-router";
 
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Hero/>
-      <Slider/>
-      <About/>
-      <Chart/>
-      <Token/>
-      <Footer/>
-    </div>
+    <>
+      <div>
+        <Header />
+        <Hero />
+        <Slider />
+        <About />
+        <Chart />
+        <Token />
+        <Footer />
+      </div>
+      {/* <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path="/webxv" element={<Web/>}/>
+        <Route path="/community" element={<Community/>}/>
+      </Routes> */}
+    </>
   );
 };
 
